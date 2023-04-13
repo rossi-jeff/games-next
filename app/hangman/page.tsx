ccff553ebce9c81f89bef8bfd28aadb18dd88d91
Jeff Rossi <jrossi@intouchgps.com>
2023-04-13 16:01:28 -0400

    image and buttons

--- app/hangman/page.tsx
+++ app/hangman/page.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import Link from 'next/link'
+import HangManDrawing from './hang-man-drawing'
+import { HangMan } from '../../types/hang-man.type'
+import HangManLetterButtons from './hang-man-letter-buttons'
 
 export default function HangManGame() {
+	const [hangMan, setHangMan] = useState<HangMan>({})
+	const correct = 'a,e,i,o,u'
+	const wrong = 'n,t,s,v'
+
+	const guess = (ev: any) => {
+		console.log(ev)
+	}
 	return (
-		<div className="hang-man-game">
+		<div className="hang-man-game m-2">
 			<h1>Hang Man</h1>
+			<HangManDrawing wrong={hangMan.Wrong || ''} />
+			<HangManLetterButtons correct={correct} wrong={wrong} guess={guess} />
 			<Link href="/hangman/scores">Scores</Link>
 		</div>
 	)
